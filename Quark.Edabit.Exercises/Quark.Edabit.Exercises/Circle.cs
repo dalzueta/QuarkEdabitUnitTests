5ddedc9ae320238aa7eb15dc7f4bcea4ddf14922
Emanuel Jara <emanueljara96@gmail.com>
2021-10-04 20:58:48 -0300

    [exercises] - add - 135 - 140

---
+++ Quark.Edabit.Exercises/Quark.Edabit.Exercises/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quark.Edabit.Exercises
+{
+    public class Circle
+    {
+        public double Radius { get; set; }
+        public Circle(double radius) => Radius = radius;
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
