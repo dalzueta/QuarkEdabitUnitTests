a20d2c2ac138513223474ada30d3c9f7414b3af4
eliosinhache <elioh93@gmail.com>
2021-10-03 22:02:16 -0300

    First 6 Activities

--- Quark.Edabit.Exercises.Test/EdabitChallengesEasyShould.cs
+++ Quark.Edabit.Exercises.Test/EdabitChallengesEasyShould.cs
@@ -24,5 +24,87 @@ namespace Quark.Edabit.Exercises.Test
         //    /// Assert
         //    //Assert.Equal(expected, result);
         //}
+
+
+        // Double_Letter
+        [Theory]
+        [InlineData("loop", true)]
+        [InlineData("yummy", true)]
+        [InlineData("orange", false)]
+        [InlineData("munchkin", false)]
+        public void Double_Letter(string arg, bool expected)
+        {
+            /// Act
+            var result = _sut.Double_Letter(arg);
+            /// Assert 
+            Assert.Equal(expected, result);
+        }
+
+        // Stuttering 
+        [Theory]
+        [InlineData("loop", "lo... lo... loop")]
+        [InlineData("yummy", "yu... yu... yummy")]
+        [InlineData("orange", "or... or... orange")]
+        [InlineData("munchkin", "mu... mu... munchkin")]
+        public void Stuttering(string arg, string expected)
+        {
+            /// Act
+            var result = _sut.Stuttering(arg);
+            /// Assert 
+            Assert.Equal(expected, result);
+        }
+
+        // Recursion_Sum 
+        [Theory]
+        [InlineData(5, 15)]
+        [InlineData(1, 1)]
+        [InlineData(12, 78)]
+        public void Recursion_Sum(int arg, int expected)
+        {
+            /// Act
+            var result = _sut.Recursion_Sum(arg);
+            /// Assert 
+            Assert.Equal(expected, result);
+        }
+
+        // File Name
+        [Theory]
+        [InlineData("C:/Projects/pil_tests/ascii/edabit.txt", "edabit.txt")]
+        [InlineData("C:/Users/johnsmith/Music/Beethoven_5.mp3", "Beethoven_5.mp3")]
+        [InlineData("ffprobe.exe", "ffprobe.exe")]
+        public void Get_File_Name(string path, string expected)
+        {
+            /// Act
+            var result = _sut.Get_File_Name(path);
+            /// Assert 
+            Assert.Equal(expected, result);
+        }
+
+        // Next Number Greater Than A and B and...
+        [Theory]
+        [InlineData(17, 8, 24)]
+        [InlineData(93, 3, 96)]
+        [InlineData(14, 11, 22)]
+        public void Number_Greater_Than_A_B(int a, int b, int expected)
+        {
+            /// Act
+            var result = _sut.Number_Greater_Than_A_B(a, b);
+            /// Assert 
+            Assert.Equal(expected, result);
+        }
+
+        // Reverse the order of a string
+        [Theory]
+        [InlineData("Hello World","dlroW olleH")]
+        [InlineData("The quick brown fox.", ".xof nworb kciuq ehT")]
+        [InlineData("Edabit is really helpful", "!lufpleh yllaer si tibadE")]
+        public void Reverse(string words, string expected)
+        {
+            /// Act
+            var result = _sut.Reverse(words);
+            /// Assert 
+            Assert.Equal(expected, result);
+        }
+
     }
 }
--- Quark.Edabit.Exercises/Quark.Edabit.Exercises/EdabitChallengesEasy.cs
+++ Quark.Edabit.Exercises/Quark.Edabit.Exercises/EdabitChallengesEasy.cs
@@ -1,10 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Quark.Edabit.Exercises
 {
     public class EdabitChallengesEasy
     {
+        public bool Double_Letter(string word)
+        {
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (word[i] == word[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string Stuttering(string word)
+        {
+            return word[0] + "" + word[1] + "... " + word[0] + word[1] + "... " + word;
+        }
+
+        public int Recursion_Sum(int num)
+        {
+            int total = 0;
+            if (num > 0)
+            {
+                total = num + Recursion_Sum(num - 1);
+            }
+            return total;
+        }
+
+        public string Get_File_Name(string path) {
+            int count = 0;
+            bool flag = false;
+            for (int i = path.Length-1; i >= 0 ; i--)
+            {
+                if (path[i] == '/') {
+                    count = i;
+                    flag = true;
+                    break;
+                }
+            }
+            return (!flag) ? path : path.Substring(count + 1);
+        }
+
+        public int Number_Greater_Than_A_B(int A, int B)
+        {
+            int num = A +1;
+            while ((num < B) || (num % B != 0))
+            {
+                num += 1;
+            }
+            return num;
+        }
+
+        public string Reverse(string words)
+        {
+            string[] splittedWords = words.Split(" ");
+            string wordsReversed = "";
+            for (int i= words.Length -1; i = 0;  i --)
+            {
+                var result = words[i].ToCharArray().Reverse();
+
+                wordsReversed += new string(result.ToArray());
+            }
+        }
     }
 }
