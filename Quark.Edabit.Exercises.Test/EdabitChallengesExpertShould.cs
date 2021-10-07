ac11859f1a10971a9306f0a3d7819b3c06e75a37
Daniel <danielalzueta@gmail.com>
2021-10-07 02:03:38 -0300

    [exercise - Expert] - add - first exercise

---
+++ Quark.Edabit.Exercises.Test/EdabitChallengesExpertShould.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Quark.Edabit.Exercises.Test
+{
+    public class EdabitChallengesExpertShould
+    {
+        private EdabitChallengesExpert _sut;
+
+        public EdabitChallengesExpertShould()
+        {
+            _sut = new EdabitChallengesExpert();
+        }
+
+        [Theory]
+        [InlineData(11211230, false)]
+        [InlineData(13001120, true)]
+        [InlineData(23336014, true)]
+        [InlineData(11, true)]
+        public void SumTwoNumbers(int number, bool expected)
+        {
+            /// Act
+            var result = _sut.palindromeDescendant(number);
+            /// Assert
+            Assert.Equal(expected, result);
+        }
+
+
+    }
+}
---
+++ Quark.Edabit.Exercises/Quark.Edabit.Exercises/EdabitChallengesExpert.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace Quark.Edabit.Exercises
+{
+    public class EdabitChallengesExpert
+    {
+        public bool palindromeDescendant(int number)
+        {
+
+            if (IsPalindromo(number))
+            {
+                return true;
+            }
+            else
+            {
+                if (number > 999)
+                {
+                    var a = ObtainNextChild(number.ToString());
+                    if (palindromeDescendant(int.Parse(a))) return true;
+
+                }
+            }
+            return false;
+        }
+
+
+        public string ObtainNextChild(string strNumber)
+        {
+            if (strNumber.Length < 3)
+            {
+                return SumNumbers(int.Parse(strNumber)).ToString();
+            }
+            else
+            {
+                var firstTwoValues = strNumber.Substring(0, 2);
+                int sumTwoFirstValues = SumNumbers(int.Parse(firstTwoValues));
+
+                return string.Join("", sumTwoFirstValues, ObtainNextChild(strNumber.Substring(2)));
+            }
+        }
+
+
+        private bool IsPalindromo(int number)
+        {
+            var reverse = string.Join("", Math.Abs(number).ToString().Reverse());
+            return number == int.Parse(reverse);
+        }
+
+        private string GetFirsTwoChars(ref string str)
+        {
+            var chars = str.Substring(0, 2);
+            str = str.Substring(2);
+            return chars;
+        }
+
+        private int SumNumbers(int num)
+        {
+            var unid = num / 10;
+            var desc = num % 10;
+
+            return unid + desc;
+
+        }
+    }
+}
